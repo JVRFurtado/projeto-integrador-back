13cfa8c1fb8a73a8f8f35d7ab7a788dbb0e91071
João Furtado <joao.furtado546@gmail.com>
2026-05-08 11:05:00 -0300

    correções

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL:", DATABASE_URL)
 
 engine = create_engine(
     DATABASE_URL,
